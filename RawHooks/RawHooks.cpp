452d4ffad41267ec977ecd93caef1460df906331
dkluin <dcluynserf@gmail.com>
2017-04-27 14:53:01 +0200

    RawHooks updates

--- RawHooks/RawHooks.cpp
+++ RawHooks/RawHooks.cpp
@@ -1,4 +1,6 @@
 #include "RawHooks.h"
+#include "../General.h"
+#include "../Injecting.h"
 
 uint8_t* TheRawHooks::ms_pTheRawHooks;
 uint32_t TheRawHooks::ms_nGlobalPos;
@@ -8,6 +10,7 @@ bool TheRawHooks::Initialize()
 	ms_pTheRawHooks = new uint8_t[THERAWHOOKS_SPACE];
 	if (ms_pTheRawHooks)
 	{
+		ms_nGlobalPos = 0;
 		return true;
 	}
 	else
@@ -18,5 +21,63 @@ bool TheRawHooks::Initialize()
 
 bool TheRawHooks::Shutdown()
 {
+	if (ms_pTheRawHooks)
+	{
+		free(ms_pTheRawHooks);
+	}
 	return true;
+}
+
+RawHook::RawHook(uint32_t m_dwJumpFrom, uint32_t m_dwJumpTo)
+{
+	this->m_dwJumpedFromAddr = m_dwJumpFrom;
+	this->m_dwJumpOutAddr = m_dwJumpTo;
+	this->m_bFinished = false;
+	this->m_bStarted = false;
+}
+
+RawHook::~RawHook()
+{
+
+}
+
+void RawHook::Write(const void* m_nData, size_t m_iSize)
+{
+	memcpy(TheRawHooks::ms_pTheRawHooks + TheRawHooks::ms_nGlobalPos, m_nData, m_iSize);
+	TheRawHooks::ms_nGlobalPos += m_iSize;
+}
+
+void RawHook::MakeRawJMP(uint32_t m_dwReturnAddr)
+{
+	MemoryInjector::MakeJMP((uint32_t)TheRawHooks::ms_pTheRawHooks + TheRawHooks::ms_nGlobalPos, m_dwReturnAddr, true); 
+	TheRawHooks::ms_nGlobalPos += 5;
+}
+
+void RawHook::MakeRawCALL(uint32_t m_dwReturnAddr)
+{
+	MemoryInjector::MakeCALL((uint32_t)TheRawHooks::ms_pTheRawHooks + TheRawHooks::ms_nGlobalPos, m_dwReturnAddr, true);
+	TheRawHooks::ms_nGlobalPos += 5;
+}
+
+void RawHook::Finish()
+{
+	if (this->m_bStarted && !this->m_bFinished)
+	{
+		this->MakeRawJMP(this->m_dwJumpOutAddr);
+		this->Write("\x90\x90", 2);
+
+		// Write a JMP instruction on the specified address to the starting position
+		MemoryInjector::MakeJMP(this->m_dwJumpedFromAddr, this->m_dwPositionOnStart, true);
+		this->m_bFinished = true;
+	}
+}
+
+void RawHook::Start()
+{
+	if (!this->m_bStarted)
+	{
+		this->m_dwPositionOnStart = TheRawHooks::ms_nGlobalPos;
+		this->Write("\x90\x90", 2);
+		this->m_bStarted = true;
+	}
 }
--- RawHooks/RawHooks.h
+++ RawHooks/RawHooks.h
@@ -1,3 +1,6 @@
+/*
+	RawHooks.h
+*/
 #pragma once
 
 #include <cstdint>
@@ -8,16 +11,30 @@
 class RawHook
 {
 public:
-	RawHook();
+	RawHook(uint32_t m_dwJumpFrom, uint32_t m_dwJumpTo);
 	~RawHook();
 
+	// Start using this hook - meaning that we set the old position before we add anything to the global position value so we can make a JMP instruction to this position inside the heap when Finish() is called
+	void Start();
+
+	// Writes a JMP to an address specified on the current raw hook
 	void MakeRawJMP(uint32_t m_dwReturnAddr);
+
+	// Same, but just calls a function
 	void MakeRawCALL(uint32_t m_dwReturnAddr);
 
-	void Write(const void* m_nData);
+	// Can be used to write raw hexadecimal instructions onto the heap space 
+	void Write(const void* m_nData, size_t m_iSize);
+
+	// Finishes with the current raw hook - this will add a JMP opcode to m_dwJumpOutAddr specified and then adds 2 NOP instructions (just to make it easier to identify patches)
+	void Finish();
+
 private:
 	uint32_t m_dwJumpedFromAddr;
 	uint32_t m_dwJumpOutAddr;
+	uint32_t m_dwPositionOnStart;
+	bool m_bFinished;
+	bool m_bStarted;
 };
 
 class TheRawHooks
@@ -25,7 +42,7 @@ class TheRawHooks
 public:
 	bool Initialize();
 	bool Shutdown();
-private:
+
 	static uint8_t* ms_pTheRawHooks;
 	static uint32_t ms_nGlobalPos;
 };
