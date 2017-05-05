7fb7ee9d633fdaa1e22b02557c4384b08cbb608b
dkluin <dcluynserf@gmail.com>
2017-05-05 16:47:51 +0200

    updated Calling to support most calling conventions
    Signed-off-by: dkluin <dcluynserf@gmail.com>

--- Calling.h
+++ Calling.h
@@ -5,9 +5,27 @@
 class Calling
 {
 public:
+	// Default calling convention
 	template<class Ret, class ...Args>
 	static Ret Call(uint32_t p, Args... a);
 
+	// __thiscall
 	template<class Ret, class ...Args>
 	static Ret ThisCall(uint32_t p, Args... a);
+
+	// __stdcall
+	template<class Ret, class ...Args>
+	static Ret StdCall(uint32_t p, Args... a);
+
+	// __cdecl
+	template<class Ret, class ...Args>
+	static Ret CDecl(uint32_t p, Args... a);
+
+	// __fastcall
+	template<class Ret, class ...Args>
+	static Ret FastCall(uint32_t p, Args... a);
+
+	// __vectorcall
+	template<class Ret, class ...Args>
+	static Ret VectorCall(uint32_t p, Args... a);
 };
