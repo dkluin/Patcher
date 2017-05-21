b009b7847790d0ec3cafe21c3c0b4f93d4377ca9
dkluin <dcluynserf@gmail.com>
2017-05-21 11:17:43 +0200

    implemented wrappers around RawHook constructors

--- RawHooks/TheRawHooks.h
+++ RawHooks/TheRawHooks.h
@@ -23,6 +23,55 @@ public:
 	static void StaticRawHook(uint32_t dwJumpFrom, std::string szBytes, uint32_t dwJumpTo);
 	static void StaticRawHook(Memory* pJumpFrom, std::string szBytes, Memory* pJumpTo);
 
+	// Wrappers around RawHookVariable constructor for laziness sake
+	static inline RawHookVariable* CreateVariable(std::string szVariableName, uint8_t bValue)
+	{
+		RawHookVariable m_Variable(szVariableName, bValue);
+		return &m_Variable;
+	}
+
+	static inline RawHookVariable* CreateVariable(const char* szVariableName, uint8_t bValue)
+	{
+		RawHookVariable m_Variable(szVariableName, bValue);
+		return &m_Variable;
+	}
+
+	static inline RawHookVariable* CreateVariable(std::string szVariableName, uint16_t wValue)
+	{
+		RawHookVariable m_Variable(szVariableName, wValue);
+		return &m_Variable;
+	}
+
+	static inline RawHookVariable* CreateVariable(const char* szVariableName, uint16_t wValue)
+	{
+		RawHookVariable m_Variable(szVariableName, wValue);
+		return &m_Variable;
+	}
+
+	static inline RawHookVariable* CreateVariable(std::string szVariableName, uint32_t dwValue)
+	{
+		RawHookVariable m_Variable(szVariableName, dwValue);
+		return &m_Variable;
+	}
+
+	static inline RawHookVariable* CreateVariable(const char* szVariableName, uint32_t dwValue)
+	{
+		RawHookVariable m_Variable(szVariableName, dwValue);
+		return &m_Variable;
+	}
+
+	static inline RawHookVariable* CreateVariable(std::string szVariableName, float fValue)
+	{
+		RawHookVariable m_Variable(szVariableName, fValue);
+		return &m_Variable;
+	}
+
+	static inline RawHookVariable* CreateVariable(const char* szVariableName, float fValue)
+	{
+		RawHookVariable m_Variable(szVariableName, fValue);
+		return &m_Variable;
+	}
+
 private:
 	// Replace all occurences of szFrom with szTo in string szTargetStr
 	static void ReplaceAllInString(std::string& szTargetStr, const std::string& szFrom, const std::string& szTo);
