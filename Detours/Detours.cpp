b371ece8a8483584088037b36874c6cdb942fb67
dkluin <dcluynserf@gmail.com>
2017-11-18 15:35:33 +0100

    added detour code

--- Detours/Detours.cpp
+++ Detours/Detours.cpp
@@ -1,2 +1,35 @@
 #include "../Shared.hpp"
 #include "Detours.h"
+#include "../Injecting.h"
+
+// Detour the function
+void Detours::DetourFunction(HMODULE hModule, std::string szExportName, void* pDest, uint64_t& qwBackup)
+{
+	uint32_t pSource = reinterpret_cast<uint32_t>(GetProcAddress(hModule, szExportName.c_str()));
+	
+	qwBackup = MemoryInjector::ReadMemory<uint64_t>(pSource);
+	MemoryInjector::MakeJMP(pSource, pDest);
+}
+
+void Detours::DetourFunction(std::string szModuleName, std::string szExportName, void* pDest, uint64_t& qwBackup)
+{
+	uint32_t pSource = reinterpret_cast<uint32_t>(GetProcAddress(GetModuleHandleA(szModuleName.c_str()), szExportName.c_str()));
+
+	qwBackup = MemoryInjector::ReadMemory<uint64_t>(pSource);
+	MemoryInjector::MakeJMP(pSource, pDest);
+}
+
+// Undo the detour and restore the value
+void Detours::UndoDetour(HMODULE hModule, std::string szExportName, const uint64_t qwBackup)
+{
+	uint32_t pSource = reinterpret_cast<uint32_t>(GetProcAddress(hModule, szExportName.c_str()));
+
+	MemoryInjector::WriteMemory<uint64_t>(pSource, qwBackup);
+}
+
+void Detours::UndoDetour(std::string szModuleName, std::string szExportName, const uint64_t qwBackup)
+{
+	uint32_t pSource = reinterpret_cast<uint32_t>(GetProcAddress(GetModuleHandleA(szModuleName.c_str()), szExportName.c_str()));
+
+	MemoryInjector::WriteMemory<uint64_t>(pSource, qwBackup);
+}
--- Detours/Detours.h
+++ Detours/Detours.h
@@ -1,9 +1,16 @@
 #pragma once
 
-#include "Detour.h"
+#include <string>
 
-// Same as Detour.h, but only uses classes internally, just for laziness purposes
+// Detours
 class Detours
 {
 public:
+	// Detour the function
+	static void DetourFunction(HMODULE hModule, std::string szExportName, void* pDest, uint64_t& qwBackup);
+	static void DetourFunction(std::string szModuleName, std::string szExportName, void* pDest, uint64_t& qwBackup);
+
+	// Undo the detour and restore the value
+	static void UndoDetour(HMODULE hModule, std::string szExportName, const uint64_t qwBackup);
+	static void UndoDetour(std::string szModuleName, std::string szExportName, const uint64_t qwBackup);
 };
