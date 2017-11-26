8dbfefe94c20ea1521f81ae082e7b125d9dc4cb4
dkluin <dcluynserf@gmail.com>
2017-11-26 21:23:06 +0100

    ....

--- Detours/Detours.cpp
+++ Detours/Detours.cpp
@@ -2,34 +2,27 @@
 #include "Detours.h"
 #include "../Injecting.h"
 
+uint32_t Detours::ms_pDetourFunctionPointers[MAX_NUM_DETOURS];
+
 // Detour the function
-void Detours::DetourFunction(HMODULE hModule, std::string szExportName, void* pDest, uint64_t& qwBackup)
+void Detours::DetourFunction(std::string szModuleName, std::string szExportName, void* pDest, uint32_t& dwBackup, uint32_t iIndexInTable)
 {
-	uint32_t pSource = reinterpret_cast<uint32_t>(GetProcAddress(hModule, szExportName.c_str()));
-	
-	qwBackup = MemoryInjector::ReadMemory<uint64_t>(pSource);
-	MemoryInjector::MakeJMP(pSource, pDest);
-}
+	uint32_t pSource = reinterpret_cast<uint32_t>(GetProcAddress(GetModuleHandleA(szModuleName.c_str()), szExportName.c_str())) + 2;
 
-void Detours::DetourFunction(std::string szModuleName, std::string szExportName, void* pDest, uint64_t& qwBackup)
-{
-	uint32_t pSource = reinterpret_cast<uint32_t>(GetProcAddress(GetModuleHandleA(szModuleName.c_str()), szExportName.c_str()));
-
-	qwBackup = MemoryInjector::ReadMemory<uint64_t>(pSource);
-	MemoryInjector::MakeJMP(pSource, pDest);
+	ms_pDetourFunctionPointers[iIndexInTable] = reinterpret_cast<uint32_t>(pDest);
+	dwBackup = MemoryInjector::ReadMemory<uint32_t>(pSource);
+	MemoryInjector::WriteMemory(pSource, ms_pDetourFunctionPointers);
 }
 
 // Undo the detour and restore the value
-void Detours::UndoDetour(HMODULE hModule, std::string szExportName, const uint64_t qwBackup)
+void Detours::UndoDetour(std::string szModuleName, std::string szExportName, const uint32_t dwBackup)
 {
-	uint32_t pSource = reinterpret_cast<uint32_t>(GetProcAddress(hModule, szExportName.c_str()));
+	uint32_t pSource = reinterpret_cast<uint32_t>(GetProcAddress(GetModuleHandleA(szModuleName.c_str()), szExportName.c_str())) + 2;
 
-	MemoryInjector::WriteMemory<uint64_t>(pSource, qwBackup);
+	MemoryInjector::WriteMemory<uint32_t>(pSource, dwBackup);
 }
 
-void Detours::UndoDetour(std::string szModuleName, std::string szExportName, const uint64_t qwBackup)
+uint32_t Detours::GetExportAddress(std::string szModuleName, std::string szExportName)
 {
-	uint32_t pSource = reinterpret_cast<uint32_t>(GetProcAddress(GetModuleHandleA(szModuleName.c_str()), szExportName.c_str()));
-
-	MemoryInjector::WriteMemory<uint64_t>(pSource, qwBackup);
+	return reinterpret_cast<uint32_t>(GetProcAddress(GetModuleHandleA(szModuleName.c_str()), szExportName.c_str()));
 }
--- Detours/Detours.h
+++ Detours/Detours.h
@@ -2,15 +2,21 @@
 
 #include <string>
 
+#define MAX_NUM_DETOURS 256
+
 // Detours
 class Detours
 {
 public:
 	// Detour the function
-	static void DetourFunction(HMODULE hModule, std::string szExportName, void* pDest, uint64_t& qwBackup);
-	static void DetourFunction(std::string szModuleName, std::string szExportName, void* pDest, uint64_t& qwBackup);
+	static void DetourFunction(std::string szModuleName, std::string szExportName, void* pDest, uint32_t& dwBackup, uint32_t iIndexInTable);
+
+	// Gets the address
+	static uint32_t GetExportAddress(std::string szModuleName, std::string szExportName);
 
 	// Undo the detour and restore the value
-	static void UndoDetour(HMODULE hModule, std::string szExportName, const uint64_t qwBackup);
-	static void UndoDetour(std::string szModuleName, std::string szExportName, const uint64_t qwBackup);
+	static void UndoDetour(std::string szModuleName, std::string szExportName, uint32_t dwBackup);
+
+	// Detour table
+	static uint32_t ms_pDetourFunctionPointers[MAX_NUM_DETOURS];
 };
