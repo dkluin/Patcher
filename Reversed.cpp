3077a5ab1d62a050a6ad33580f7115b26eb9b44b
dkluin <dr.kluin@student.alfa-college.nl>
2018-11-11 08:34:29 +0100

    using file logging instead

--- Reversed.cpp
+++ Reversed.cpp
@@ -2,6 +2,9 @@
 #include "Reversed.h"
 
 std::vector<ReversedFunctionInfo> Reversed::ms_vReversedFunctions;
+FILE* Reversed::ms_pFile = nullptr;
+bool Reversed::ms_bLoggingOverlappingPatches = false;
+int Reversed::ms_nDetectedMatches = 0;
 
 void Reversed::CheckIfAddressIsInRange(uint32_t dwAddress)
 {
@@ -10,14 +13,45 @@ void Reversed::CheckIfAddressIsInRange(uint32_t dwAddress)
 	{
 		if (dwAddress >= ms_vReversedFunctions.at(i).m_dwStartAddress && dwAddress <= ms_vReversedFunctions.at(i).m_dwEndAddress)
 		{
-			PatcherPrintMessageBox("Patch at address 0x%X overlaps with reversed function between 0x%X and 0x%X", dwAddress, ms_vReversedFunctions.at(i), ms_vReversedFunctions.at(i).m_dwEndAddress);
+			if (ms_bLoggingOverlappingPatches)
+			{
+				fprintf(ms_pFile, "Possible patch at address 0x%X overlaps with reversed function named %s (between 0x%X and 0x%X)\n", dwAddress, ms_vReversedFunctions.at(i).m_szFunctionName, ms_vReversedFunctions.at(i).m_dwStartAddress, ms_vReversedFunctions.at(i).m_dwEndAddress);
+				ms_nDetectedMatches++;
+			}
 		}
 	}
 #endif
 }
 
-ReversedFunctionInfo::ReversedFunctionInfo(uint32_t dwStart, uint32_t dwEnd)
+void Reversed::StartLogging()
 {
+#ifdef PATCHER_DEVELOPMENT_BUILD
+	if (!ms_bLoggingOverlappingPatches)
+	{
+		ms_pFile = fopen("overlapping_patches.log", "w");
+		ms_bLoggingOverlappingPatches = true;
+	}
+#endif
+}
+
+void Reversed::StopLogging()
+{
+#ifdef PATCHER_DEVELOPMENT_BUILD
+	if (ms_bLoggingOverlappingPatches)
+	{
+		fclose(ms_pFile);
+		ms_bLoggingOverlappingPatches = false;
+		ms_nDetectedMatches = 0;
+
+		PatcherPrintMessageBox("Stopped detecting overlapping patches - %d matches found and saved to overlapping_patches.log", ms_nDetectedMatches);
+	}
+#endif
+}
+
+
+ReversedFunctionInfo::ReversedFunctionInfo(char* szFunctionName, uint32_t dwStart, uint32_t dwEnd)
+{
+	strcpy(m_szFunctionName, szFunctionName);
 	m_dwStartAddress = dwStart;
 	m_dwEndAddress = dwEnd;
 
--- Reversed.h
+++ Reversed.h
@@ -11,16 +11,22 @@ class Reversed
 
 private:
 	static std::vector<ReversedFunctionInfo> ms_vReversedFunctions;
+	static FILE* ms_pFile;
+	static bool ms_bLoggingOverlappingPatches;
+	static int ms_nDetectedMatches;
 
 public:
 	static void CheckIfAddressIsInRange(uint32_t dwAddress);
+	static void StartLogging();
+	static void StopLogging();
 };
 
 class ReversedFunctionInfo
 {
 public:
 	uint32_t m_dwStartAddress, m_dwEndAddress;
+	char m_szFunctionName[250];
 
-	ReversedFunctionInfo() { m_dwStartAddress = 0; m_dwEndAddress = 0; }
-	ReversedFunctionInfo(uint32_t dwStart, uint32_t dwEnd);
+	ReversedFunctionInfo() { m_dwStartAddress = 0; m_dwEndAddress = 0; memset(m_szFunctionName, 0, sizeof(m_szFunctionName)); }
+	ReversedFunctionInfo(char* szFunctionName, uint32_t dwStart, uint32_t dwEnd);
 };
